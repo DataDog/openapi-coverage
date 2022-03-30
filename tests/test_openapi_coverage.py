25028dd3d3516efe4ef1e8e517a3ac1c707a7249
NouemanKHAL <noueman.khal@gmail.com>
2022-03-30 23:49:45 +0200

    Add primitive types tests (#4)

--- tests/test_openapi_coverage.py
+++ tests/test_openapi_coverage.py
@@ -1,11 +1,37 @@
 from openapi_coverage import cover_schema, coverable_parts
 
-def test_cover_schema():
+def test_cover_schema_primitive_types():
     schema={
         "type": "string"
     }
     result = list(cover_schema(schema, "test"))
     assert result[0] == tuple(("", True))
+    result = list(cover_schema(schema, 1))
+    assert result[0] == tuple(("", False))
+
+    schema={
+        "type": "integer"
+    }
+    result = list(cover_schema(schema, 1))
+    assert result[0] == tuple(("", True))
+    result = list(cover_schema(schema, "1"))
+    assert result[0] == tuple(("", False))
+
+    schema={
+        "type": "number"
+    }
+    result = list(cover_schema(schema, 1.0))
+    assert result[0] == tuple(("", True))
+    result = list(cover_schema(schema, "1.0"))
+    assert result[0] == tuple(("", False))
+
+    schema={
+        "type": "boolean"
+    }
+    result = list(cover_schema(schema, True))
+    assert result[0] == tuple(("", True))
+    result = list(cover_schema(schema, "True"))
+    assert result[0] == tuple(("", False))
 
 
 def test_coverable_parts():
