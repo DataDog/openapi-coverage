7819a737f359ed0f71b86d049790071613035d11
NouemanKHAL <noueman.khal@gmail.com>
2022-03-31 17:12:39 +0200

    add cover percentage tests

--- tests/test_openapi_coverage.py
+++ tests/test_openapi_coverage.py
@@ -48,3 +48,52 @@ def test_coverable_parts():
     }
     result = coverable_parts(schema)
     assert result == {"a", "b"}
+
+def test_cover_all():
+    schema={
+        "type": "object",
+        "properties": {
+            "a": {
+                "type": "string"
+            },
+            "b": {
+                "type": "integer"
+            }
+        }
+    }
+
+    data={
+        "a": "hello",
+        "b": 85
+    }
+
+    total = coverable_parts(schema)
+    actual = cover_schema(schema, data)
+    coverage_percentage = len(total & actual)/len(total)
+
+    assert coverage_percentage == 1.0
+
+
+def test_partial_cover():
+    schema={
+        "type": "object",
+        "properties": {
+            "a": {
+                "type": "string"
+            },
+            "b": {
+                "type": "integer"
+            }
+        }
+    }
+
+    data={
+        "a": "hello",
+        "b": "85"
+    }
+
+    total = coverable_parts(schema)
+    actual = cover_schema(schema, data)
+    coverage_percentage = len(total & actual)/len(total)
+
+    assert coverage_percentage == 0.5
