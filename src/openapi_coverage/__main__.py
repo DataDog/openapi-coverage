deb010b61aafcd9a5abb57743cdf1247a99de83d
Jiri Kuncar <jiri.kuncar@gmail.com>
2022-04-01 14:15:24 -0400

    Expand coverable paths (#16)
    * Expand coverable paths
    
    * format coverage per operation

--- src/openapi_coverage/__main__.py
+++ src/openapi_coverage/__main__.py
@@ -5,8 +5,9 @@ from jsonref import JsonRef
 from yaml import CSafeLoader
 
 from .har import cover_har, load_har
-from .paths import build_url_map
+from .paths import build_url_map, coverable_paths
 from .refs import replace_refs
+from .trie import build_trie
 
 schema_path = sys.argv[1]
 
@@ -20,5 +21,20 @@ for har_path in sys.argv[2:]:
     har = load_har(har_path)
     coverred |= cover_har(schema, har, url_map=url_map)
 
-for c in sorted({replace_refs(schema, c) for c in coverred}):
-    print(c)
+coverable = coverable_paths(schema)
+coverable_trie = build_trie(coverable)
+coverred_trie = build_trie(coverred)
+
+# for c in sorted({replace_refs(schema, c) for c in coverred}):
+#     print(c)
+
+for operation in sorted({p[:3] for p in coverable}):
+    c = coverred_trie.count_children(operation)
+    ca = coverable_trie.count_children(operation)
+    if c > 0:
+        r = 100.0 * c / ca
+    else:
+        r = 0
+    print(f"{r:>6.2f}% - {operation[1:]}")
+
+print(len(coverred)/len(coverable))
