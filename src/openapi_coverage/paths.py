3fe83b4bbcf0941d19b7c786c96e1a37fdf8e498
NouemanKHAL <noueman.khal@gmail.com>
2022-04-01 19:45:11 +0200

    Add coverable_paths function (#15)

--- src/openapi_coverage/paths.py
+++ src/openapi_coverage/paths.py
@@ -18,3 +18,20 @@ def build_url_map(schema):
             # TODO handle servers
 
     return Map(rules)
+
+
+from openapi_coverage.schemas import coverable_parts
+
+
+def coverable_paths(schema, schema_keys=None):
+    """Return schema parts that should be tested."""
+
+    coverage = set()
+
+    for path, operations in schema.get("paths", {}).items():
+        for method, operation in operations.items():
+            if "parameters" in operation:
+                for param in operation["parameters"]:
+                    coverage |= coverable_parts(param["schema"], [param["name"]])
+
+    return coverage
