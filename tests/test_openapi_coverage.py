75138593c0e5c7e405e7fa21fd78f656b0557cc6
Jiri Kuncar <jiri.kuncar@gmail.com>
2022-04-01 11:16:19 -0400

    Add support for replacing `$refs` (#12)

--- tests/test_openapi_coverage.py
+++ tests/test_openapi_coverage.py
@@ -1,5 +1,6 @@
 from openapi_coverage.schemas import cover_schema, coverable_parts
 from openapi_coverage.paths import build_url_map
+from openapi_coverage.refs import replace_refs
 
 
 def test_cover_schema_primitive_types():
@@ -34,48 +35,56 @@ def test_coverable_parts():
         "properties": {"a": {"type": "string"}, "b": {"type": "integer"}},
     }
     result = coverable_parts(schema)
-    assert result == {("a",), ("b",)}
+    assert result == {
+        (
+            "properties",
+            "a",
+        ),
+        (
+            "properties",
+            "b",
+        ),
+    }
+
 
 def test_cover_all():
-    schema={
+    schema = {
         "type": "object",
         "properties": {"a": {"type": "string"}, "b": {"type": "integer"}},
     }
-    data={
-        "a": "hello",
-        "b": 85
-    }
+    data = {"a": "hello", "b": 85}
 
     total = coverable_parts(schema)
     actual = cover_schema(schema, data)
-    coverage_percentage = len(total & actual)/len(total)
+    coverage_percentage = len(total & actual) / len(total)
 
     assert coverage_percentage == 1.0
 
 
 def test_partial_cover():
-    schema={
+    schema = {
         "type": "object",
         "properties": {"a": {"type": "string"}, "b": {"type": "integer"}},
     }
 
-    data={
-        "a": "hello",
-        "b": "85"
-    }
+    data = {"a": "hello", "b": "85"}
 
     total = coverable_parts(schema)
     actual = cover_schema(schema, data)
-    coverage_percentage = len(total & actual)/len(total)
+    coverage_percentage = len(total & actual) / len(total)
 
     assert coverage_percentage == 0.5
+
 
 def test_schemas(load_yaml, load_json, dereference):
     schema = dereference(load_yaml("schemas/allOf.yaml"))
     dog = load_json("data/allOf/valid/dog.json")
 
     result = cover_schema(schema["components"]["schemas"]["Dog"], dog)
-    assert result == {("allOf", 0, "pet_type"), ("allOf", 1, "bark")}
+    assert result == {
+        ("allOf", 0, "properties", "pet_type"),
+        ("allOf", 1, "properties", "bark"),
+    }
 
 
 def test_url_map(load_yaml, dereference):
@@ -85,4 +94,30 @@ def test_url_map(load_yaml, dereference):
 
     assert map_adapter.match("/pets", "GET") == (("paths", "/pets", "get"), {})
     assert map_adapter.match("/pets", "POST") == (("paths", "/pets", "post"), {})
-    assert map_adapter.match("/pets/42", "GET") == (("paths", "/pets/{petId}", "get"), {"petId": "42"})
+    assert map_adapter.match("/pets/42", "GET") == (
+        ("paths", "/pets/{petId}", "get"),
+        {"petId": "42"},
+    )
+
+
+def test_replace_refs(load_yaml, dereference):
+    schema = dereference(load_yaml("schemas/allOf.yaml"))
+    dog = {
+        "pet_type": "Dog",
+        "bark": True,
+        "breed": "Labrador",
+    }
+
+    result = {
+        replace_refs(schema, c)
+        for c in cover_schema(
+            schema["components"]["schemas"]["Dog"],
+            dog,
+            schema_keys=["components", "schemas", "Dog"],
+        )
+    }
+    assert result == {
+        ("components", "schemas", "Dog", "allOf", 1, "properties", "bark"),
+        ("components", "schemas", "Dog", "allOf", 1, "properties", "breed"),
+        ("components", "schemas", "Pet", "properties", "pet_type"),  # from allOf $ref
+    }
