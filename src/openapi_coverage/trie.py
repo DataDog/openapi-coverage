17c0fe56cc5a3d5b30de3aa5ebbde0ea1ae02ba0
NouemanKHAL <noueman.khal@gmail.com>
2022-04-01 17:16:48 +0200

    add trie

---
+++ src/openapi_coverage/trie.py
@@ -0,0 +1,60 @@
+
+from typing import Dict, Set, List
+
+class TrieNode():
+    def __init__(self, value: str, children: Dict):
+        self.value = value
+        self.children = children or dict()
+
+class Trie():
+    def __init__(self):
+        self.root = TrieNode("root", None)
+        self.empty = True
+
+    def insert(self, path):
+        current = self.root
+        for k in path:
+            if k not in current.children:
+                current.children[k] = TrieNode(k, None)
+            current = current.children[k]
+
+    def get(self, path):
+        current = self.root
+        for k in path:
+            if k not in current.children:
+                return None
+            current = current.children[k]
+        return current
+
+
+    def count_children(self, path):
+        root = self.get(path)
+
+        if not root:
+            return -1
+
+        cnt = 0
+
+
+        q = [root]
+
+        # BFS
+        while q:
+            cur = q[0]
+            print(f"path:{path} root:{root.value}")
+            q.pop(0)
+
+            cnt += len(cur.children)
+
+            for k, v in cur.children.items():
+                print(f"k:{k} v:{v.value}")
+                q.append(v)
+
+        return cnt
+
+
+def build_trie(coverage: List) -> Trie:
+    trie = Trie()
+    for path in coverage:
+        trie.insert(path)
+    return trie
